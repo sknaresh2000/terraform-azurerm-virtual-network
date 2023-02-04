6d4268ca8ba855cc4d9a11cad1e5b660be373b36
NareshKumaran Sathiaseelan <naresh@nareshkransmini.mynetworksettings.com>
2023-02-04 09:43:36 -0500

    Initial

---
+++ outputs.tf
@@ -0,0 +1,4 @@
+output "name" {
+  value       = azurerm_virtual_network.vnet.name
+  description = "The name of the virtual network"
+}
---
+++ variables.tf
@@ -0,0 +1,30 @@
+variable "location" {
+  type        = string
+  description = "The Azure region where the virtual network should exist. Changing this forces a new resource to be created."
+  default     = "eastus"
+}
+
+variable "name" {
+  type        = string
+  description = "The name of the virtual network to create"
+}
+
+variable "rg_name" {
+  type        = string
+  description = "The name of the resource group where this virtual network needs to reside"
+}
+
+variable "address_space" {
+  type        = list(string)
+  description = "The address space of the virtual network"
+}
+
+variable "dns_servers" {
+  type        = list(string)
+  description = "DNS server details incase of using custom dns servers"
+}
+
+variable "tags" {
+  type        = map(any)
+  description = "A mapping of tags to assign to the resource"
+}
